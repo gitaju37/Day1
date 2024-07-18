4973b6893e7ecd9a81992acdceab2b676d39425d
gitaju37 <ajithachumech23@gmail.com>
2024-07-18 19:20:54 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use( cors() );
+const PORT = 4000 
+
+
+app.get( '/first', (req, res) => {
+    res.status(200).json({message:"wellcome back to codding world"})
+})
+
+
+
+
+app.listen( PORT, () => {
+    console.log( "APP is listening in the port:", PORT );
+})
