18880abeb39f5ca2f8b447204edddaaef394ee9d
gitaju37 <ajithachumech23@gmail.com>
2024-07-19 13:37:51 +0530

    added

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ import cors from 'cors';
 
 const app = express();
 app.use( cors() );
-const PORT = 4000 
+const PORT = 4000
 
 
-app.get( '/first', (req, res) => {
-    res.status(200).json({message:"wellcome back to codding world"})
-})
+app.get( '/first', ( req, res ) => {
+    res.status( 200 ).json( { message: "wellcome back to codding world" } )
+} )
 
 
 
 
 app.listen( PORT, () => {
     console.log( "APP is listening in the port:", PORT );
-})
+} )
